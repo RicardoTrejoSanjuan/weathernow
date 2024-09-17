05f8386bc80039bdc3040bfd20060d5adf6ee7d8
RicardoTrejoSanjuan <trejosanjuanricardo@gmail.com>
2024-09-16 19:28:33 -0600

    first commit 🌦️

---
+++ README.md
@@ -0,0 +1,3 @@
+# WeatherNow
+
+WeatherNow is a React application that allows you to quickly and easily check the current weather for any city. It provides up-to-date information on temperature, humidity, wind speed, and more, giving you a comprehensive overview of the weather conditions wherever you are.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-4xl font-bold">WeatherNow</h1>
+      <p>WeatherNow is a React application that allows you to quickly and easily check the current weather for any city. It provides up-to-date information on temperature, humidity, wind speed, and more, giving you a comprehensive overview of the weather conditions wherever you are.</p>
+    </div>
+  )
+}
+
+export default App
